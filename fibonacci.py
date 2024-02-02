9cd50932c281d3888195b1eabe51e5b44fe37dbc
madghost41 <stephenmicheal97@gmail.com>
2024-02-02 12:10:13 -0600

    second commit

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,17 @@
 def fibonacci(n):
-  pass
+  previous = 0
+  current = 1
+  next = 0
+  if n == 0:
+    return 0
+  
+  for x in range(n):
+    next = previous + current 
+    previous == current
+    current == next 
+      
+    return current
+
+
 
 
